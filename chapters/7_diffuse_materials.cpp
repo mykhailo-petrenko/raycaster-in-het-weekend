e282d376389bf0341ab5485723ae0be3540e9672
Mykhailo Petrenko <mikael.petrenko@gmail.com>
2022-02-25 00:43:24 +0100

    Simple diffuse material.

--- chapters/7_diffuse_materials.cpp
+++ chapters/7_diffuse_materials.cpp
@@ -8,6 +8,27 @@
 
 namespace chapter7 {
 
+    vec3 random_unit_sphere() {
+        vec3 unit;
+
+        do {
+            unit = vec3(drand48(), drand48(), drand48()) * 2.f - 1.f;
+        } while (length(unit) >= 1);
+
+        return unit;
+    }
+
+    vec3 calcColor(Ray &ray, Hittable *world) {
+        Hit hit;
+        if (world->hit(ray, 0.0001, MAXFLOAT, hit)) {
+            vec3 target = hit.point + hit.normal + random_unit_sphere();
+            auto reflection = Ray(hit.point, target - hit.point);
+            return 0.5f * calcColor(reflection, world);
+        } else {
+            return lerp(WHITE, BLUE, (ray.direction.y + 1.) / 2);
+        }
+    }
+
     /**
      * Surface Normals
      */
@@ -22,7 +43,6 @@ namespace chapter7 {
 
         image << "P3\n" << NX << " " << NY << "\n255\n";
 
-        color3 rgb;
         Ray ray;
         auto camera = new Camera();
 
@@ -35,7 +55,6 @@ namespace chapter7 {
 //        list[3] = new Sphere(vec3(-0.5, 0.5, -0.75), 0.25);
 
         auto *world = new HittableList(list, OBJECTS_COUNT);
-        Hit hit;
 
         for (int y = NY - 1; y >= 0; y--) {
             for (int x = 0; x < NX; x++) {
@@ -47,20 +66,15 @@ namespace chapter7 {
 
                     ray = camera->getRay(u, v);
 
-                    if (world->hit(ray, 0, MAXFLOAT, hit)) {
-                        color += project_normal_to_color(hit.normal);
-                    } else {
-                        color += lerp(WHITE, BLUE, (ray.direction.y + 1.) / 2);
-                    }
+                    color += calcColor(ray, world);
                 }
 
                 color /= float(NS);
-
+                color = vec3(sqrt(color[0]), sqrt(color[1]), sqrt(color[2]));
                 color3 rgb = project_color_vector(color);
                 image << rgb[0] << " " << rgb[1] << " " << rgb[2] << "\n";
             }
         }
-
 
         image.close();
     }
